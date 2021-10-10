80ae9ab5433a3f3b4508f40aca9b96741458a0d3
jiangyanwei <jiangyanwei@meituan.com>
2021-10-11 00:35:56 +0800

    Initial commit

---
+++ src/test/java/com/jiangyanwei/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiangyanwei.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
